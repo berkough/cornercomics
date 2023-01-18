82e176866e07f584b0b3cf74c69df4b81995fbcf
Mathew Kane <berkough@gmail.com>
2023-01-17 21:48:48 -0800

    not much has changed

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ let comic = 'x-men';
 let url = `https://metron.cloud/api/series/?name=${comic}`
 
 let req = fetch(url,{
-    crossDomain: true
+    credentials: 'include',
+    mode: 'no-cors',
+    headers: {
+        'Authentication':'Basic',
+    }
 })
     .then(response => {
         console.log(response);
-        JSON.parse(response);
     });
 
 console.log(req);
