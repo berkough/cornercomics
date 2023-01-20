07af9a02bd4cc5a64d90c9db17b20502b1791a1e
Mathew <berkough@gmail.com>
2023-01-19 23:49:14 -0800

    comics can be added to a list now

--- index.js
+++ index.js
@@ -1,40 +1,74 @@
-const url = `proxy.php?url=https://metron.cloud/api`;
-let issueBlock = ``;
-let seriesModal = document.getElementById('series-list');
-let seriesModalTitle = document.getElementById('series-modal-title');
+/**
+ * CornerComics Issue Tracker
+ */
 
+//Global Variables.
+const URL = `https://metron.cloud/api`; //API url
+
+const comicModalBody = document.getElementById('comic-modal-body');
+const comicModalBodyTitle = document.getElementById('comic-modal-title');
+const comicList = document.getElementById('comic-list');
+
+//Start the functions.
 async function callComicModal(){
     //Pulls a list of series based on a search term.
-    let comic = document.getElementById('searchInp').value;
-    await fetch(`${url}/series/?format=json&name=${comic}`,{
-    }).then((response) => response.json())
+    let comic = document.getElementById('searchInp').value; //Grab the value in the search box.
+    comicModalBody.innerHTML = ''; //Clear the innerHTML for subsequent searches.
+    //Fetch the list of comic series based on your search phrase:
+    await fetch(`${URL}/series/?format=json&name=${comic}`)
+        .then((response) => response.json())
         .then((data) => {
             console.log(data);
+            //For each comic series in the json data return a link that passes in 
             for (const comicSeries of data.results){
                 let comicSeriesItem = `<a href="#" onclick="getComicIssues(${comicSeries.id});return false;" 
-                        data-target="#comic-issue-modal">${comicSeries.series}</a><br>`;
-                seriesModal.innerHTML += comicSeriesItem;
+                        data-target="#comic-modal-body">${comicSeries.series}</a><br>`;
+                comicModalBody.innerHTML += comicSeriesItem;
             }
         });
 }
 
 async function getComicIssues(id){
-    //Pulls a list of issues for the selected series.
-    let comicIssueModal = document.getElementById('issue-list');
-    await fetch(`${url}/series/${id}/issue_list`).then((response)=>response.json())
+    comicModalBody.innerHTML = '';
+    await fetch(`${URL}/series/${id}/issue_list`)
+        .then((response)=>response.json())
         .then((data)=>{
             console.log(data);
-            for (const issue of data.results){
-                comicIssueItem = `${issue.issue}`;
-                seriesModal.innerHTML = '';
-                seriesModalTitle.innerHTML = 'Pick an issue to add to your list:';
-                //seriesModal.innerHTML += issueBlock;
+            for (const comicIssue of data.results){
+                let comicIssueItem = `<a href="#" onclick="saveIssue(${comicIssue.id});return false;">${comicIssue.issue}</a><br>`;
+                comicModalBodyTitle.innerHTML = 'Pick an issue to add to your list:';
+                comicModalBody.innerHTML += comicIssueItem;
             }
         });
 }
 
-function saveIssue(){
-    //Saves a comic issue to the list.
+//Saves a comic issue to the list.
+async function saveIssue(id){
+    await fetch(`${URL}/issue/${id}`)
+        .then((response)=>response.json())
+        .then((data)=>{
+            console.log(data);
+            comicList.innerHTML += `<div class="container-fluid border border-primary">
+                                        <div class="row">
+                                            <div class="col-3 align-self-start"><img src="${data.image}" width="80%" alt=""><br>
+                                            <p>${data.series.name}<br>Issue #${data.number}<br>Date: ${data.store_date}</p>
+                                            </div>
+                                            <div class="col-6">
+                                                <p>${data.name}</p><br>
+                                                <p>${data.desc}</p>
+                                            </div>
+                                            <div class="col-3">
+                                                <button class="btn btn-primary">Edit</button><br>
+                                                <button class="btn btn-primary">Delete</button>
+                                            </div>
+                                            <div class="col-12 input-group">
+                                                <span class="input-group-text primary-outline">Notes</span>
+                                                 <textarea class="form-control primary-outline" aria-label="Notes"></textarea>
+                                            </div>
+                                        </div>
+                                    </div>`;
+
+        });
 }
 
 function deleteIssue(){
